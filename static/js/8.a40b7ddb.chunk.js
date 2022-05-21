840bc49d9974ed87d4e52b18bbd89424b51e4ad4
khushpreet-kaur <khushpreetk363@gmail.com>
2022-05-21 01:53:46 +0000

    Updates

---
+++ static/js/8.a40b7ddb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppersonal-site"]=this["webpackJsonppersonal-site"]||[]).push([[8],{170:function(e,t,s){"use strict";s.r(t);var r=s(0),i=(s(1),s(5)),n=s(21);t.default=function(){return Object(r.jsx)(n.a,{title:"Blog",description:"Khushpreet Kaur's Blog.",children:Object(r.jsxs)("div",{children:[Object(r.jsx)("h1",{"data-testid":"heading",children:"Sorry! Blog is currently down!"}),Object(r.jsxs)("p",{children:["Return ",Object(r.jsx)(i.b,{to:"/",children:"home"}),"."]})]})})}}}]);
+//# sourceMappingURL=8.a40b7ddb.chunk.js.map
